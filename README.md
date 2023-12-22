1e7c6714654ea713c55efafbb4cb7002e66a05b8
Kristo Aun <kristoaun@live.com>
2023-12-22 13:18:47 +0200

    k3s dev cluster

---
+++ README.md
@@ -0,0 +1,16 @@
+# iac-lxd-dev-k3s
+
+DEV cluster
+
+## Move Kubernetes config file to Gitlab runner
+
+    lxc exec container-adm-glrunner-k1 -- bash -c 'mkdir -p /home/gitlab-runner/.kube'
+    lxc file push kube_config.yml container-adm-glrunner-k1/home/gitlab-runner/.kube/config
+    lxc exec container-adm-glrunner-k1 -- bash -c 'chown gitlab-runner: /home/gitlab-runner/.kube'
+    
+Then
+    
+    lxc exec container-adm-glrunner-k1 -- /bin/bash
+    su - gitlab-runner
+    kubectl get pods --all-namespaces
+
---
+++ main.tf
@@ -0,0 +1,48 @@
+locals {
+  cluster_domain          = "k3s-cluster.${var.env}.acme.corp"
+  profile_privileged_name = "k3s-privileged-${var.env}"
+  container_profiles = [
+    "limits",
+    "fs-dir",
+    "nw-${var.env}",
+    local.profile_privileged_name
+  ]
+  containers_master = [
+    {
+      name         = "container-${var.env}-k3s-m1"
+      ipv4_address = "10.0.20.151"
+      profiles     = local.container_profiles
+    }
+  ]
+  containers_worker = [
+    {
+      name         = "container-${var.env}-k3s-w1"
+      ipv4_address = "10.0.20.154"
+      profiles     = local.container_profiles
+    }
+  ]
+}
+
+module "lxd_k3s_privileged_profile" {
+  source = "github.com/studio-telephus/terraform-lxd-k3s-privileged-profile.git?ref=1.0.0"
+  name   = local.profile_privileged_name
+}
+
+module "lxd_k3s_cluster" {
+  source            = "github.com/studio-telephus/terraform-lxd-k3s-cluster.git?ref=1.0.0"
+  swarm_private_key = var.swarm_private_key
+  cluster_domain    = local.cluster_domain
+  nicparent         = "${var.env}-network"
+  cidr_pods         = "10.21.0.0/16"
+  cidr_services     = "10.22.0.0/16"
+  containers_master = local.containers_master
+  containers_worker = local.containers_worker
+  autostart         = true
+  depends_on        = [module.lxd_k3s_privileged_profile]
+}
+
+resource "local_file" "kube_config" {
+  content    = module.lxd_k3s_cluster.k3s_kube_config
+  filename   = var.kube_config_path
+  depends_on = [module.lxd_k3s_cluster]
+}
