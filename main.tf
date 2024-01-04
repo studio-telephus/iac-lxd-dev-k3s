2708c2b8719648c96a13de969571158182b63d6b
Kristo Aun <kristoaun@live.com>
2024-01-04 02:59:38 +0200

    dev

--- main.tf
+++ main.tf
@@ -38,9 +38,11 @@ module "lxd_k3s_cluster" {
   global_flags = [
     "--write-kubeconfig-mode 644",
     "--disable=traefik",
-    "--kube-controller-manager-arg bind-address=0.0.0.0",
     "--kube-proxy-arg metrics-bind-address=0.0.0.0",
-    "--kube-scheduler-arg bind-address=0.0.0.0"
+    "--kube-controller-manager-arg address=0.0.0.0",
+    "--kube-controller-manager-arg bind-address=0.0.0.0",
+    "--kube-scheduler-arg bind-address=0.0.0.0",
+    "--kube-scheduler-arg address=0.0.0.0"
   ]
   containers_master = local.containers_master
   containers_worker = local.containers_worker
